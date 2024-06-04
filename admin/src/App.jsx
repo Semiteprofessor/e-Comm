8f357718c6d0bb6e75b3c7b5c39e8f42fd501801
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-04 21:59:12 +0100

    Commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,9 +4,9 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  Redirect,
+  Navigate,
 } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
@@ -18,9 +18,45 @@ import Login from "./pages/login/Login";
 import { useSelector } from "react-redux";
 
 function App() {
+  const admin = useSelector((state) => state.user.currentUser.isAdmin);
   return (
     <>
-      <Chart />
+      <Router>
+        <Routes>
+          <Route path="/login">
+            <Login />
+          </Route>
+          {admin && (
+            <>
+              <Topbar />
+              <div className="container">
+                <Sidebar />
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/users">
+                  <UserList />
+                </Route>
+                <Route path="/user/:userId">
+                  <User />
+                </Route>
+                <Route path="/newUser">
+                  <NewUser />
+                </Route>
+                <Route path="/products">
+                  <ProductList />
+                </Route>
+                <Route path="/product/:productId">
+                  <Product />
+                </Route>
+                <Route path="/newproduct">
+                  <NewProduct />
+                </Route>
+              </div>
+            </>
+          )}
+        </Routes>
+      </Router>
     </>
   );
 }
