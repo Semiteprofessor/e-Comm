e9b08fcb2e60ad810d3fcbf3d63c58a3dccd319e
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-16 21:41:39 +0100

    Commit

---
+++ admin/src/pages/product/Product.jsx
@@ -0,0 +1,59 @@
+import { Link, useLocation } from "react-router-dom";
+import "./product.css";
+import Chart from "../../components/chart/Chart";
+import { productData } from "../../dummyData";
+import { Publish } from "@material-ui/icons";
+import { useSelector } from "react-redux";
+import { useEffect, useMemo, useState } from "react";
+import { userRequest } from "../../requestMethods";
+
+const Product = () => {
+  const location = useLocation();
+  const productId = location.pathname.split("/")[2];
+  const [pStats, setPStats] = useState([]);
+
+  const product = useSelector((state) =>
+    state.product.products.find((product) => product._id === productId)
+  );
+
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get("orders/income?pid=" + productId);
+        const list = res.data.sort((a, b) => {
+          return a._id - b._id;
+        });
+        list.map((item) =>
+          setPStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], Sales: item.total },
+          ])
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getStats();
+  }, [productId, MONTHS]);
+  return <div>Product</div>;
+};
+
+export default Product;
