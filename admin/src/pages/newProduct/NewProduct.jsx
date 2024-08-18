19a7f01843520198ec45d9ae207e2bddaaa2978d
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-18 21:34:58 +0100

    Commit

---
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./newProduct.css";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import app from "../../firebase";
+import { addProduct } from "../../redux/apiCalls";
+import { useDispatch } from "react-redux";
+
+const NewProduct = () => {
+  const [inputs, setInputs] = useState({});
+  const [file, setFile] = useState(null);
+  const [cat, setCat] = useState([]);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+  const handleCat = (e) => {
+    setCat(e.target.value.split(","));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const fileName = new Date().getTime() + file.name;
+    const storage = getStorage(app);
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+  return <div>NewProduct</div>;
+};
+
+export default NewProduct;
