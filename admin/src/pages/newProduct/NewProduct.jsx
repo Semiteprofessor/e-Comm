b85c8b3e044abb3a09213b3e3dd43b8553f275b0
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-19 21:36:28 +0100

    Commit

--- admin/src/pages/newProduct/NewProduct.jsx
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -68,7 +68,62 @@ const NewProduct = () => {
     );
   };
 
-  return <div>NewProduct</div>;
+  return (
+    <div className="newProduct">
+      <h1 className="addProductTitle">New Product</h1>
+      <form className="addProductForm">
+        <div className="addProductItem">
+          <label>Image</label>
+          <input
+            type="file"
+            id="file"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Title</label>
+          <input
+            name="title"
+            type="text"
+            placeholder="Apple Airpods"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Description</label>
+          <input
+            name="desc"
+            type="text"
+            placeholder="description..."
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Price</label>
+          <input
+            name="price"
+            type="number"
+            placeholder="100"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Categories</label>
+          <input type="text" placeholder="jeans,skirts" onChange={handleCat} />
+        </div>
+        <div className="addProductItem">
+          <label>Stock</label>
+          <select name="inStock" onChange={handleChange}>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+        </div>
+        <button onClick={handleClick} className="addProductButton">
+          Create
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default NewProduct;
