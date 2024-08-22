477cf386071d93ceb12ce9b0422c1cd460b81729
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-22 21:25:56 +0100

    Commit

---
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const WidgetSm = () => {
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+        {users.map((user) => (
+          <li className="widgetSmListItem" key={user._id}>
+            <img
+              src={
+                user.img ||
+                "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+              }
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default WidgetSm
