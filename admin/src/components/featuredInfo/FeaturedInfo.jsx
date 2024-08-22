ef8f98fb64579003b08f4acd5c28b21a75568265
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-22 21:14:46 +0100

    Commit

--- admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -12,7 +12,9 @@ const FeaturedInfo = () => {
         const res = await userRequest.get("orders/income");
         setIncome(res.data);
         setPerc((res.data[1].total * 100) / res.data[0].total - 100);
-      } catch {}
+      } catch (err) {
+        console.log(err);
+      }
     };
     getIncome();
   }, []);
