3cae5d0cc169d77acec401200e3b0c945c0dbe88
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-24 22:56:35 +0100

    Commit

--- api/cores/cart.core.js
+++ api/cores/cart.core.js
@@ -20,10 +20,73 @@ const addToCart = async (req, res) => {
 };
 
 const updateCart = async (req, res) => {
-    const cartId = 
-    try {
-        
-    } catch (error) {
-        
-    }
+  const cartId = req.params._id;
+  try {
+    await Cart.findByIdAndUpdate(cartId, { $set: req.body }, { new: true });
+    res.status(200).json({
+      status: true,
+      message: "Cart updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: true,
+      message: "Error updating cart",
+    });
+  }
+};
+
+const deleteCart = async (req, res) => {
+  const cartId = req.params._id;
+  try {
+    await Cart.findByIdAndDelete(cartId);
+    res.status(200).json({
+      status: true,
+      message: "Cart deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: true,
+      message: "Error deleting cart",
+    });
+  }
+};
+
+const getCart = async (req, res) => {
+  const cartId = req.params._id;
+  try {
+    const cart = Cart.findById(cartId);
+
+    res.status(200).json({
+      status: true,
+      message: "Single Cart fetched successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: true,
+      message: "Error fetching single cart",
+    });
+  }
+};
+
+const getCarts = async (req, res) => {
+  try {
+    const carts = Cart.find();
+    res.status(200).json({
+      status: true,
+      message: "Cart fetched successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: true,
+      message: "Error fetching cart",
+    });
+  }
+};
+
+module.exports = {
+  addToCart,
+  updateCart,
+  deleteCart,
+  getCart,
+  getCarts,
 };
