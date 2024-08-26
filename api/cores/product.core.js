9f6228f7310d455ad389e5bb7ead106ad5bf1cf1
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-26 21:40:50 +0100

    Commit

---
+++ api/cores/product.core.js
@@ -0,0 +1,13 @@
+const productModel = require("../models/product.model")
+
+const createProduct = (req, res) => {
+    try {
+    const {title, dec, img, category, }  = req.body;
+    const newProduct = productModel.create({
+
+    })
+        const 
+    } catch (error) {
+        
+    }
+}
