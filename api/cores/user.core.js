8c62f57d893947779c1d182d8147a1c9e7f63397
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-28 16:13:36 +0100

    Commit

--- api/cores/user.core.js
+++ api/cores/user.core.js
@@ -1,1 +1,50 @@
-const 
+const User = require("../models/user.model");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("../utils/verifyToken.util");
+
+const updateUser = async (req, res, next) => {
+  const { password } = req.body;
+  if (password) {
+    password = CryptoJS.AES.encrypt(
+      password,
+      process.env.PASS_SECRET
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params._id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params._id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getSingleUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params._id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getSingleUser = async (req, res, next) => {};
