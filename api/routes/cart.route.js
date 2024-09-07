d8b8713290658151bdfb3c0a32b6ef5d9b6843c7
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-07 19:41:12 +0100

    Commit

---
+++ api/routes/cart.route.js
@@ -0,0 +1,29 @@
+const {
+  verifyToken,
+  verifyTokenAndAdmin,
+  verifyTokenAndAuthorization,
+} = require("../utils/verifyToken.util");
+const {
+  addToCart,
+  deleteCart,
+  updateCart,
+  getCarts,
+  getCart,
+} = require("../cores/cart.core");
+
+const router = require("express").Router();
+
+// CREATE CART ROUTE
+router.post("/add", verifyToken, addToCart);
+
+// UPDATE CART ROUTE
+router.post("/update/:id", verifyTokenAndAuthorization, updateCart);
+
+// DELETE CART ROUTE
+router.delete("/delete/:id", verifyTokenAndAuthorization, deleteCart);
+
+// GET CART ROUTE
+router.get("/find/:id", verifyTokenAndAuthorization, getCart);
+
+// GET CARTS ROUTE
+router.get("/", verifyTokenAndAdmin, getCarts);
